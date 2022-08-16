6cd64a73ed0e4091e43cc8c8cb25f0575a988c69
Charles Turner <ct6g18@soton.ac.uk>
2022-08-16 11:05:42 +0100

    Setup.py updates

--- setup.py
+++ setup.py
@@ -10,9 +10,11 @@ setup(
   ],
   install_requires=["numpy","pandas","shutil","os","warmings","scipy","lmfit","matplotlib","openpyxl"]
   description="This is the cleaned up organic-alkalinity-sausage-machine, now known more sensibly as OrgAlkCalc",
+  py_modules=["SausageMachine"],
+  package_dir={'': 'src'},
   license="MIT license",
   include_package_data=True,
-  name='hello',
+  name='OrgAlkCalc',
   version='0.1.0',
   zip_safe=False,
 ):
