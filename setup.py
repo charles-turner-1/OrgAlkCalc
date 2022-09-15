943faf54b267ba1e12f0d947267397127168dbc5
Charles Turner <ct6g18@soton.ac.uk>
2022-09-15 15:29:29 +0100

    Minor cleanup in OrgAlkCalc

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
+with open("README.md","r") as fh:
+  long_description = fh.read()
+
 setup(
   author="Charles Turner",
   author_email='charlesturner0987@gmail.com',
@@ -8,8 +11,8 @@ setup(
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3',
   ],
-  install_requires=["numpy","pandas","scipy","lmfit" ,"matplotlib"
-  ,"openpyxl"],
+  install_requires=["numpy","pandas","scipy","lmfit" ,"matplotlib" ,"openpyxl"],
+  extras_require = {"dev": ["pytest>=3.7",],},
   description="This is the cleaned up organic-alkalinity-sausage-machine, now known more sensibly as OrgAlkCalc",
   py_modules=["OrgAlkCalc"],
   package_dir={'': 'src'},
@@ -18,4 +21,6 @@ setup(
   name='OrgAlkCalc',
   version='0.1.0',
   zip_safe=False,
+  long_description=long_description,
+  long_description_content_type="text/markdown",
 )
