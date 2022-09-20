87e67e0a0dc3f21b9f2ea501ee0eccaacb0cef8d
Charles Turner <ct6g18@soton.ac.uk>
2022-09-20 18:51:46 +0100

    Updated documentation for PyPi

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
   license="MIT license",
   include_package_data=True,
   name='OrgAlkCalc',
-  version='0.1.0',
+  version='0.1.1',
   zip_safe=False,
   long_description=long_description,
   long_description_content_type="text/markdown",
